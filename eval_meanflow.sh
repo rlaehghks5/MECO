4cf07b60d437a1804919a7f853f0c4273e7651ad
Dohwan Kim <121927513+rlaehghks5@users.noreply.github.com>
2026-06-09 15:07:58 +0900

    Update eval_meanflow.sh

--- eval_meanflow.sh
+++ eval_meanflow.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=3 python evaluate.py --folder_destination /home/dohwan/MeanFlowSE_GeCO/results/MeanFlow_results  --ckpt .ckpt
+python evaluate.py --folder_destination /MeanFlowSE/results/MeanFlow_results  --ckpt .ckpt
